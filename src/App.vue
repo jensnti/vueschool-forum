b525d7039764cf4932bacf032d9650a387493d1c
jensnti <jens.andreasson@ga.ntig.se>
2021-03-27 14:26:49 +0100

    chap 3 done

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,14 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <div class="container">
-    <page-home />
+    <router-view />
   </div>
 </template>
 
 <script>
-import PageHome from '@/components/PageHome.vue';
-// import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
-  components: {
-    PageHome,
-  },
 };
 </script>
 
